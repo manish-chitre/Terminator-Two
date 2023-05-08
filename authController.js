46c56399287e9cf0898b88da342369190d34b3b5
Manish Chitre <manish_chitre@hotmail.com>
2023-05-08 10:40:55 +0530

    verification of token

--- authController.js
+++ authController.js
@@ -1,3 +1,11 @@
 function Authentication(){
     console.log('This is authentication example');
 }
+
+function getToken(){
+    console.log('This is get token function');
+}
+
+function verifyToken(){
+    console.log('This is verification of token');
+}
